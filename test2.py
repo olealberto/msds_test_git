8d5a36577f0ddbba1a94dab52580c3dbe8a47e5f
Ubuntu <ubuntu@ip-172-31-16-71.ec2.internal>
2026-01-17 00:21:21 +0000

    Update HTML and Python server files

--- test2.py
+++ test2.py
@@ -2,9 +2,9 @@ import http.server
 import socketserver
 
 PORT = 8002
-
 Handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer(("ec2-35-89-201-160.us-west-2.compute.amazonaws.com", PORT), Handler) as httpd:
-    print("serving at port", PORT)
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Serving on port {PORT}")
     httpd.serve_forever()
+
